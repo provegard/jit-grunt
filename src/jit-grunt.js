14e5a10ab5983cc2b9ca41075ce53b653812180d
Per Rovegård <per@rovegard.se>
2018-09-29 23:55:43 +0200

    Add TypeScript/.ts as supported plugin extension

--- src/jit-grunt.js
+++ src/jit-grunt.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const PREFIXES = ['', 'grunt-', 'grunt-contrib-'];
-const EXTENSIONS = ['.coffee', '.js'];
+const EXTENSIONS = ['.coffee', '.js', '.ts'];
 
 const jit = {
   pluginsRoot: 'node_modules',
